1b02281e737c71e1149eed5ed7c4a920c8e9ee4a
yeeter <81702700+yeeterlol@users.noreply.github.com>
2025-02-06 16:19:29 -0800

    improve shit

--- BPM/checksum.py
+++ BPM/checksum.py
@@ -1,17 +1,16 @@
-def basic_hash(name):
+def bkdr(name):
     res = 0
     for ch in name:
         c = ord(ch)
         # force uppercase any characters
-        # the assembly never lies ¯\_(ツ)_/¯
         if 'a' <= ch <= 'z':
             c -= 0x20 
         res = (res * 31 + c) & 0xffffffff
     return res 
 
-def get_ks_sum(name, items):
-    checksum = csum(name)
-    res = checksum % items
+def packman_sum(name, items):
+    hash = bkdr(name)
+    res = hash % items
     print("hash -> ", res)
     print("offset -> ", ((res+1) * 16))
     return res
